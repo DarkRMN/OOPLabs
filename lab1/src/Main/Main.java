c6973160d0f6b98f50d9ac8c7734a7c3308c93c2
Roman <Kopyt2004Roman@gmail.com>
2023-04-17 16:57:45 +0300

    lab1 refactor

--- lab1/src/Main/Main.java
+++ lab1/src/Main/Main.java
@@ -1,58 +1,71 @@
 package Main;
+
 import Car.Car;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Main {
-    private Car[] cars;
+    private final Car[] cars;
 
     public static void main(String[] args) {
         Main main = new Main();
 
-
-        main.showModel();
-        main.showYear();
-        main.showPrice();
+        main.filterByModel();
+        main.filterByYear();
+        main.filterByPrice();
     }
 
-    public Main(){
+    public Main() {
         cars = new Car[2];
 
-        cars[0] = new Car(1, "Nissan", 1999, 11, "BO6969AA");
+        cars[0] = new Car(1, "Nissan", 1999, BigDecimal.valueOf(11), "BO6969AA");
         cars[1] = new Car();
     }
 
-    public void showModel(){
+    public void filterByModel() {
         System.out.println("-------------------------------------------");
         System.out.println("Enter car model:");
         Scanner scanner = new Scanner(System.in);
         String carModel = scanner.nextLine();
-        for(Car item : cars){
+        filterByModel(carModel);
+    }
+
+    public void filterByModel(String carModel) {
+        for (Car item : cars) {
             if (item.getModel().equals(carModel))
                 System.out.printf(item.toString());
-
         }
     }
 
-    public void showYear(){
+    public void filterByYear() {
         System.out.println("-------------------------------------------");
         System.out.println("Enter car production year:");
         Scanner scanner = new Scanner(System.in);
         int carYear = scanner.nextInt();
-        for (Car item: cars){
-            if (item.getYear() < carYear)
-                System.out.println(item.toString());
+        scanner.nextLine();
+        filterByYear(carYear);
+    }
+
+    private void filterByYear(int carYear) {
+        for (Car item : cars) {
+            if (item.getYear() > carYear)
+                System.out.println(item);
         }
     }
 
-    public void showPrice(){
+    public void filterByPrice() {
         System.out.println("-------------------------------------------");
         System.out.println("Enter price of the car:");
         Scanner scanner = new Scanner(System.in);
         double carPrice = scanner.nextDouble();
-        for (Car item: cars){
-            if (item.getPrice() < carPrice)
-                System.out.println(item.toString());
+        filterByPrice(BigDecimal.valueOf(carPrice));
+    }
+
+    private void filterByPrice(BigDecimal carPrice) {
+        for (Car item : cars) {
+            if (item.getPrice().compareTo(carPrice) < 0)
+                System.out.println(item);
         }
     }
 }
